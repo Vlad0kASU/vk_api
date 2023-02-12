b7b7b49fc693f33811d09404efae586d98a49dea
Vlad0kASU <vlad0k.asu@yandex.ru>
2023-02-12 16:10:22 +0300

    убраны лишние комментарии в test.py test.py переименован на wallPost.py добавлен файл addFriens.py который позволяет отправлять заявки в друзья и принимать входящие добавлен файл main.py, который управяляет этими процессами автоматически

---
+++ main.py
@@ -0,0 +1,18 @@
+from addFriens import addFriens
+from wallPost import wallPost
+from time import sleep
+from datetime import datetime
+
+def main():
+    print('Начало работы')
+    addFriens()
+    while True:
+        date = datetime.now()
+        time = str(date)[11:]
+        if time[:2] in ['11', '13']:
+            wallPost(1)
+        sleep(3600)
+
+
+if __name__ == '__main__':
+    main()
